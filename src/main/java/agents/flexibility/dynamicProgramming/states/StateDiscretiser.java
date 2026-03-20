0871452f1dc196617c93a2b39a7fc570bdcb652a
Christoph Schimeczek <Christoph.Schimeczek@dlr.de>
2026-03-20 14:28:48 +0100

    :memo:(StateDiscretizer) add javadoc

--- src/main/java/agents/flexibility/dynamicProgramming/states/StateDiscretiser.java
+++ src/main/java/agents/flexibility/dynamicProgramming/states/StateDiscretiser.java
@@ -111,12 +111,13 @@ public class StateDiscretiser {
 				: allocateEnergyStates();
 	}
 
-	/** Updates list of all states - use only, if {@link #considerTimeConstraint} applies */
+	/** Updates list of all states - use only, if {@link #considerTimeConstraint} applies; <br>
+	 * adds a single balanced state at shift time 0 plus all unbalanced states for non-zero shift times */
 	private int[] allocateEnergyAndTimeStates(int lowestEnergyState, int highestEnergyState) {
 		int[] states = new int[calcStateCount(highestEnergyState - lowestEnergyState + 1, numberOfTimeStates)];
 		states[0] = energyStateOffset;
 		int arrayIndex = 1;
-		for (int time = 1; time < numberOfTimeStates; time++) {
+		for (int time = 1; time < numberOfTimeStates; time++) { // add unbalanced states for non-zero shift times
 			for (int energy = lowestEnergyState; energy <= highestEnergyState; energy++) {
 				if (energy == energyStateOffset) {
 					continue;
